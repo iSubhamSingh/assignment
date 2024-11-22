7e7fa3fa948b56da6ebf987926b19c5626ad0381
Subham <subhamsingh5638@gmail.com>
2024-11-22 14:20:04 +0530

    modified script

--- week-3/easy/quiz-app/script.js
+++ week-3/easy/quiz-app/script.js
@@ -5,8 +5,17 @@ let i = 0;
 while(i < quizData.length){
     let question = document.getElementById('header');
     question.innerText = quizData[i].question;
+    let option1 = document.getElementById('option1');
+    option1.innerText = quizData[i].a;
+
     i++;
     
 }
+
+
+let option1 = document.getElementById('option1');
+    option1.textContent = quizData[0].a;
+    console.log(quizData[0].a);
+    
 
 
